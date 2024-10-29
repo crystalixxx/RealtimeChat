9533d5087acb603a344405f8dd1dda6fdc28fd1d
Maxim Drugov <midrugov@edu.hse.ru>
2024-10-29 20:14:41 +0300

    written start.sh to start app

---
+++ app/scripts/start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Exit in case of error
+set -e
+
+docker compose build
+
+sleep 1;
+
+# Build and run containers
+docker compose up
+
+# Hack to wait for postgres container to be up before running alembic migrations
+
+sleep 5;
+
+docker compose run --rm backend alembic upgrade head

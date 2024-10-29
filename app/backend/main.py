5471f38aa9909b5a7d5abbaadfa9d231eb6d7c87
Maxim Drugov <midrugov@edu.hse.ru>
2024-10-29 20:12:48 +0300

    moved some files (in this commit they deleted)

--- app/backend/main.py
+++
@@ -1,8 +0,0 @@
-import uvicorn
-from fastapi import FastAPI
-
-app = FastAPI(title="GreenChat", docs_url="/api/docs", openapi_url="/api")
-
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8888, reload=True)

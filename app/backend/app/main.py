1e67520e0ac7a15c616f69470d81e4b7ecee6d3e
Maxim Drugov <midrugov@edu.hse.ru>
2024-11-01 21:37:25 +0300

    added middleware to app

--- app/backend/app/main.py
+++ app/backend/app/main.py
@@ -2,10 +2,22 @@ import uvicorn
 from fastapi import FastAPI
 
 from app.api.v1.main_routers import api_router
+from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="GreenChat", docs_url="/api/docs", openapi_url="/api")
 app.include_router(api_router)
 
+origins = [
+    "https://localhost:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", reload=True, port=8888)
